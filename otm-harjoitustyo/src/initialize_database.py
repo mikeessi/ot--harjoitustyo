9310a25507362974a9e8c561e1ccbbd37685cd8e
Mikko Anttonen <mikkohj.anttonen@gmail.com>
2021-12-22 23:01:31 +0200

    bugfix

--- otm-harjoitustyo/src/initialize_database.py
+++ otm-harjoitustyo/src/initialize_database.py
@@ -2,7 +2,7 @@ import sqlite3
 
 def get_db_connection():
 
-    db_connection = sqlite3.connect("data/hiscores.db")
+    db_connection = sqlite3.connect("src/data/hiscores.db")
     db_connection.isolation_level = None
 
     return db_connection
